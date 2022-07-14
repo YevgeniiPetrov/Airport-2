0d24fcb154dc656cb839d79db9d3973ab423c013
Yevgenii Petrov <petrov.yevgenii@gmail.com>
2022-07-15 00:01:26 +0300

    change TerminalRep and add FlightRep

--- Terminal/src/main/java/com/petrov/airport/repository/TerminalRepository.java
+++ Terminal/src/main/java/com/petrov/airport/repository/TerminalRepository.java
@@ -2,5 +2,8 @@ package com.petrov.airport.repository;
 
 import com.petrov.airport.entity.Terminal;
 
+import java.util.Optional;
+
 public interface TerminalRepository extends GenericRepository<Terminal> {
+    Optional<Terminal> getWithFlights(int id);
 }
--- Terminal/src/main/java/com/petrov/airport/repository/impl/TerminalRepositoryImpl.java
+++ Terminal/src/main/java/com/petrov/airport/repository/impl/TerminalRepositoryImpl.java
@@ -1,18 +1,27 @@
 package com.petrov.airport.repository.impl;
 
 import com.petrov.airport.dao.GenericDAO;
+import com.petrov.airport.dao.TerminalDAO;
 import com.petrov.airport.entity.Terminal;
 import com.petrov.airport.repository.TerminalRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 @AllArgsConstructor
 public class TerminalRepositoryImpl implements TerminalRepository {
     private GenericDAO<Terminal> genericDAO;
+    private TerminalDAO terminalDAO;
 
     @Override
     public GenericDAO<Terminal> getGenericDAO() {
         return genericDAO;
+    }
+
+    @Override
+    public Optional<Terminal> getWithFlights(int id) {
+        return terminalDAO.getWithFlights(id);
     }
 }
