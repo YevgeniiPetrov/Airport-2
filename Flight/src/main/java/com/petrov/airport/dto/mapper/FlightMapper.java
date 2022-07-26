19c67be38a92ed1b9349270cd3962b97eff35ff9
Yevgenii Petrov <petrov.yevgenii@gmail.com>
2022-07-26 21:04:52 +0300

    [flight module] change DTOs and mappers - add ResponseFlightWithAirlinesDTO and change FLightMapper

--- Flight/src/main/java/com/petrov/airport/dto/mapper/FlightMapper.java
+++ Flight/src/main/java/com/petrov/airport/dto/mapper/FlightMapper.java
@@ -2,6 +2,7 @@ package com.petrov.airport.dto.mapper;
 
 import com.petrov.airport.dto.RequestEntityDTO;
 import com.petrov.airport.dto.ResponseFlightDTO;
+import com.petrov.airport.dto.ResponseFlightWithAirlinesDTO;
 import com.petrov.airport.entity.Flight;
 
 import java.util.List;
@@ -10,4 +11,5 @@ public interface FlightMapper {
     ResponseFlightDTO flightToMap(Flight flight);
     List<ResponseFlightDTO> flightsToMap(List<Flight> flights);
     Flight mapToFlight(RequestEntityDTO requestEntityDTO);
+    ResponseFlightWithAirlinesDTO flightWithAirlinesToMap(Flight flight);
 }
