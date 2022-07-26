f13d749e3ecd0d845286d4d6405e3123c7a096e5
Yevgenii Petrov <petrov.yevgenii@gmail.com>
2022-07-26 20:54:55 +0300

    [flight module] change FlightRepository and FlightRepositoryImpl - add method getWithAirlines

--- Flight/src/main/java/com/petrov/airport/repository/impl/FlightRepositoryImpl.java
+++ Flight/src/main/java/com/petrov/airport/repository/impl/FlightRepositoryImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @AllArgsConstructor
@@ -29,5 +30,10 @@ public class FlightRepositoryImpl implements FlightRepository {
     @Override
     public List<Flight> getAllByRoute(Route route) {
         return flightDAO.getAllByRoute(route);
+    }
+
+    @Override
+    public Optional<Flight> getWithAirlines(int id) {
+        return flightDAO.getWithAirlines(id);
     }
 }
