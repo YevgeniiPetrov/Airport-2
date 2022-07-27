cb63300b92b4d8c06de370e5ea7f43cfe4a88557
Yevgenii Petrov <petrov.yevgenii@gmail.com>
2022-07-27 21:56:35 +0300

    [employee module] add entities

---
+++ Employee/src/main/java/com/petrov/airport/entity/Essence.java
@@ -0,0 +1,7 @@
+package com.petrov.airport.entity;
+
+public abstract class Essence {
+    public abstract Integer getId();
+    public abstract Boolean getRemoved();
+    public abstract void setRemoved(Boolean removed);
+}
