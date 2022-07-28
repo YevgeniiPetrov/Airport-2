49623ff9a95992711db56d109f9296274081c808
Yevgenii Petrov <petrov.yevgenii@gmail.com>
2022-07-28 21:33:58 +0300

    [employee module] add dto packeage with classes + change EmployeeService

---
+++ Employee/src/main/java/com/petrov/airport/dto/mapper/impl/EmployeeMapperImpl.java
@@ -0,0 +1,22 @@
+package com.petrov.airport.dto.mapper.impl;
+
+import com.petrov.airport.dto.ResponseEmployeeDTO;
+import com.petrov.airport.dto.impl.ResponseEmployeeDTOImpl;
+import com.petrov.airport.dto.mapper.EmployeeMapper;
+import com.petrov.airport.entity.Employee;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@NoArgsConstructor
+public class EmployeeMapperImpl implements EmployeeMapper {
+    @Override
+    public ResponseEmployeeDTO employeeToMap(Employee employee) {
+        return ResponseEmployeeDTOImpl.builder()
+                .id(employee.getId())
+                .firstName(employee.getFirstName())
+                .lastName(employee.getLastName())
+                .birthdate(employee.getBirthdate())
+                .build();
+    }
+}
--- Employee/src/main/java/com/petrov/airport/service/impl/EmployeeServiceImpl.java
+++ Employee/src/main/java/com/petrov/airport/service/impl/EmployeeServiceImpl.java
@@ -1,6 +1,21 @@
 package com.petrov.airport.service.impl;
 
+import com.petrov.airport.dto.ResponseEmployeeDTO;
+import com.petrov.airport.dto.mapper.EmployeeMapper;
+import com.petrov.airport.repository.EmployeeRepository;
 import com.petrov.airport.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+
+@Service
+@AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
+    private EmployeeRepository employeeRepository;
+    private EmployeeMapper employeeMapper;
+
+    @Override
+    public ResponseEmployeeDTO get(int id) {
+        return employeeMapper.employeeToMap(employeeRepository.get(id).get());
+    }
 }
