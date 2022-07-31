48c0444d2add38b99b5f4e22fb447590bd302f79
Yevgenii Petrov <petrov.yevgenii@gmail.com>
2022-07-31 15:37:59 +0300

    [employee module] change PostService - add method get

--- Employee/src/main/java/com/petrov/airport/service/PostService.java
+++ Employee/src/main/java/com/petrov/airport/service/PostService.java
@@ -1,4 +1,7 @@
 package com.petrov.airport.service;
 
+import com.petrov.airport.entity.Post;
+
 public interface PostService {
+    Post get(int id);
 }
--- Employee/src/main/java/com/petrov/airport/service/impl/PostServiceImpl.java
+++ Employee/src/main/java/com/petrov/airport/service/impl/PostServiceImpl.java
@@ -1,6 +1,18 @@
 package com.petrov.airport.service.impl;
 
+import com.petrov.airport.entity.Post;
+import com.petrov.airport.repository.PostRepository;
 import com.petrov.airport.service.PostService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@AllArgsConstructor
 public class PostServiceImpl implements PostService {
+    private PostRepository postRepository;
+
+    @Override
+    public Post get(int id) {
+        return postRepository.get(id).get();
+    }
 }
