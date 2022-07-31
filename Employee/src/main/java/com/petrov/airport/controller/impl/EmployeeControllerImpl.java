7f08ff4692e6522032513aff61cf584aa5388669
Yevgenii Petrov <petrov.yevgenii@gmail.com>
2022-07-31 15:15:59 +0300

    [employee module] change EmloyeeController - add metho add

--- Employee/src/main/java/com/petrov/airport/controller/impl/EmployeeControllerImpl.java
+++ Employee/src/main/java/com/petrov/airport/controller/impl/EmployeeControllerImpl.java
@@ -1,10 +1,13 @@
 package com.petrov.airport.controller.impl;
 
 import com.petrov.airport.controller.EmployeeController;
+import com.petrov.airport.dto.RequestEmployeeDTO;
+import com.petrov.airport.dto.ResponseCompleted;
 import com.petrov.airport.dto.ResponseEmployeeDTO;
 import com.petrov.airport.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +20,11 @@ public class EmployeeControllerImpl implements EmployeeController {
     @GetMapping("/employee/get")
     public ResponseEmployeeDTO get(@RequestParam int id) {
         return employeeService.get(id);
+    }
+
+    @Override
+    @PostMapping("/employee/add")
+    public ResponseCompleted add(RequestEmployeeDTO requestEmployeeDTO) {
+        return employeeService.add(requestEmployeeDTO);
     }
 }
