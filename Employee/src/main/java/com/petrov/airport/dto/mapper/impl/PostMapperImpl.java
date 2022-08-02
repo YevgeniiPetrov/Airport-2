ef3d03c64ad54b42c8b7b9ce043ac215635df46f
Yevgenii Petrov <petrov.yevgenii@gmail.com>
2022-08-02 21:26:02 +0300

    [employee module] change PostMapper - add method postWithEmployeeToMap

--- Employee/src/main/java/com/petrov/airport/dto/mapper/impl/PostMapperImpl.java
+++ Employee/src/main/java/com/petrov/airport/dto/mapper/impl/PostMapperImpl.java
@@ -2,21 +2,40 @@ package com.petrov.airport.dto.mapper.impl;
 
 import com.petrov.airport.dto.RequestPostDTO;
 import com.petrov.airport.dto.ResponsePostDTO;
+import com.petrov.airport.dto.ResponsePostWithEmployeesDTO;
 import com.petrov.airport.dto.impl.ResponsePostDTOImpl;
+import com.petrov.airport.dto.impl.ResponsePostWithEmployeesDTOImpl;
+import com.petrov.airport.dto.mapper.EmployeeMapper;
 import com.petrov.airport.dto.mapper.PostMapper;
 import com.petrov.airport.entity.Post;
-import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.stream.Collectors;
+
 @Component
-@NoArgsConstructor
+@AllArgsConstructor
 public class PostMapperImpl implements PostMapper {
+    private EmployeeMapper employeeMapper;
+
     @Override
     public ResponsePostDTO postToMap(Post post) {
         return ResponsePostDTOImpl.builder()
                 .id(post.getId())
                 .title(post.getTitle())
                 .salary(post.getSalary())
+                .build();
+    }
+
+    @Override
+    public ResponsePostWithEmployeesDTO postWithEmployeesToMap(Post post) {
+        return ResponsePostWithEmployeesDTOImpl.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .salary(post.getSalary())
+                .employees(post.getEmployees().stream()
+                        .map(employeeMapper::employeeToMap)
+                        .collect(Collectors.toList()))
                 .build();
     }
 
