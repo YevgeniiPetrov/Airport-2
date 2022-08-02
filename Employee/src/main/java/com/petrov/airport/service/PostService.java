a8d15b172cb9de896535448d3d9774566bada1f4
Yevgenii Petrov <petrov.yevgenii@gmail.com>
2022-08-02 21:28:25 +0300

    [employee module] change PostMapper - add method getWithEmployees

--- Employee/src/main/java/com/petrov/airport/service/PostService.java
+++ Employee/src/main/java/com/petrov/airport/service/PostService.java
@@ -3,8 +3,10 @@ package com.petrov.airport.service;
 import com.petrov.airport.dto.RequestPostDTO;
 import com.petrov.airport.dto.ResponseCompleted;
 import com.petrov.airport.dto.ResponsePostDTO;
+import com.petrov.airport.dto.ResponsePostWithEmployeesDTO;
 
 public interface PostService {
     ResponsePostDTO get(int id);
     ResponseCompleted add(RequestPostDTO requestPostDTO);
+    ResponsePostWithEmployeesDTO getWithEmployees(int id);
 }
