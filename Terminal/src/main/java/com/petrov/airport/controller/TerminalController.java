67ebcf22565e1711843527f4a66a19bad2837854
Yevgenii Petrov <petrov.yevgenii@gmail.com>
2022-08-03 21:52:49 +0300

    [terminal module] change TerminalController - add method add

--- Terminal/src/main/java/com/petrov/airport/controller/TerminalController.java
+++ Terminal/src/main/java/com/petrov/airport/controller/TerminalController.java
@@ -1,9 +1,13 @@
 package com.petrov.airport.controller;
 
+import com.petrov.airport.dto.RequestTerminalDTO;
+import com.petrov.airport.dto.ResponseCompleted;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 public interface TerminalController {
     List<Integer> getAllFlightIds(@RequestParam int id);
+    ResponseCompleted add(@RequestBody RequestTerminalDTO requestTerminalDTO);
 }
--- Terminal/src/main/java/com/petrov/airport/controller/impl/TerminalControllerImpl.java
+++ Terminal/src/main/java/com/petrov/airport/controller/impl/TerminalControllerImpl.java
@@ -1,9 +1,13 @@
 package com.petrov.airport.controller.impl;
 
 import com.petrov.airport.controller.TerminalController;
+import com.petrov.airport.dto.RequestTerminalDTO;
+import com.petrov.airport.dto.ResponseCompleted;
 import com.petrov.airport.service.TerminalService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,5 +21,11 @@ public class TerminalControllerImpl implements TerminalController {
     @GetMapping("/terminal/flights/get")
     public List<Integer> getAllFlightIds(int id) {
         return terminalService.getAllFlightIds(id);
+    }
+
+    @Override
+    @PostMapping("/terminal/add")
+    public ResponseCompleted add(@RequestBody RequestTerminalDTO requestTerminalDTO) {
+        return terminalService.add(requestTerminalDTO);
     }
 }
