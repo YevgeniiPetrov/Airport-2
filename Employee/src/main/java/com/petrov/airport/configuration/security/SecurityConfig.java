8a11a6b112676561cb3832cc9a6fadd535cdd7b5
Yevgenii Petrov <petrov.yevgenii@gmail.com>
2022-08-12 01:02:14 +0300

    [employee module] inprogress

--- Employee/src/main/java/com/petrov/airport/configuration/security/SecurityConfig.java
+++ Employee/src/main/java/com/petrov/airport/configuration/security/SecurityConfig.java
@@ -26,7 +26,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .formLogin()
+                .loginPage("/auth/login").permitAll()
+                .defaultSuccessUrl("/auth/success");
 
     }
 
--- Employee/src/main/java/com/petrov/airport/controller/advice/ControllerAdvice.java
+++ Employee/src/main/java/com/petrov/airport/controller/advice/ControllerAdvice.java
@@ -1,9 +1,11 @@
 package com.petrov.airport.controller.advice;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 
 import javax.persistence.NoResultException;
 
 public interface ControllerAdvice {
     ResponseEntity<String> handleNoResultException(NoResultException exception);
+    String exception(final Throwable throwable, final Model model);
 }
