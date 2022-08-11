284586975164ab750061f65ea32fc59af44e9d4b
Yevgenii Petrov <petrov.yevgenii@gmail.com>
2022-08-11 21:09:31 +0300

    [employee module] add entity Permission

--- Employee/src/main/java/com/petrov/airport/entity/Role.java
+++ Employee/src/main/java/com/petrov/airport/entity/Role.java
@@ -1,6 +1,23 @@
 package com.petrov.airport.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Getter
 public enum Role {
-    ADMIN,
-    USER;
+    ADMIN(Set.of(Permission.READ, Permission.WRITE)),
+    USER(Set.of(Permission.WRITE));
+
+    private final Set<Permission> permissions;
+
+    public Set<SimpleGrantedAuthority> getAuthorities() {
+        return getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+    }
 }
