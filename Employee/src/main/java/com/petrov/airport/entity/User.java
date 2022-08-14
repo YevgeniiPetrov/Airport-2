f9c20366e999d8d9bddf49e9f891d1e63d79fb82
Yevgenii Petrov <petrov.yevgenii@gmail.com>
2022-08-14 15:09:19 +0300

    [employee module] add entity User

---
+++ Employee/src/main/java/com/petrov/airport/entity/User.java
@@ -0,0 +1,46 @@
+package com.petrov.airport.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Builder
+@Getter
+@ToString
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Integer id;
+    private String email;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @ToString.Exclude
+    private String password;
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+    @ToString.Exclude
+    private Boolean removed;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
